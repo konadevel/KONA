7d8a2e2286c1bba3d19c390ed7f0ea934dd727ef
Unknown <konadevel@gmail.com>
2018-05-01 17:15:10 +0200

    fix for splashscreen

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -27,7 +27,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
 {
     // set reference point, paddings
     int paddingLeft = 14;
-    int paddingTop = 470;
+    int paddingTop = 450;
     int titleVersionVSpace = 17;
     int titleCopyrightVSpace = 32;
 
@@ -48,7 +48,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     pixmap = networkStyle->getSplashImage();
 
     QPainter pixPaint(&pixmap);
-    pixPaint.setPen(QColor(255, 255, 255));
+    pixPaint.setPen(QColor(0, 0, 0));
 
     // check font size and drawing with
     pixPaint.setFont(QFont(font, 28 * fontFactor));
