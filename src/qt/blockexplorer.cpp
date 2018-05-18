4621b1287400fab840c24f22daf9cbc30f40ae0b
Unknown <konadevel@gmail.com>
2018-05-18 19:57:46 +0200

    colour fix

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -550,7 +550,7 @@ void BlockExplorer::setBlock(CBlockIndex* pBlock)
 
 void BlockExplorer::setContent(const std::string& Content)
 {
-    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#FFB005;}\n a, span { font-family: monospace; }\n span.addr {color:#FFB005; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #FFB005;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#FFB005;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#FFB005;}\n";
+    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#6D6D6D;}\n a, span { font-family: monospace; }\n span.addr {color:#6D6D6D; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #6D6D6D;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#6D6D6D;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#6D6D6D;}\n";
     QString FullContent = "<html><head><style type=\"text/css\">" + CSS + "</style></head>" + "<body>" + Content.c_str() + "</body></html>";
     // printf(FullContent.toUtf8());
 
