7be24fae64ebb8eed9755746c2c35d1c0f2fc8ba
Unknown <konadevel@gmail.com>
2018-06-01 21:54:07 +0200

    bump protocol version

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80002;
+static const int PROTOCOL_VERSION = 80003;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,7 +22,7 @@ static const int GETHEADERS_VERSION = 80001;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 80001;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 80002;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 80003;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
